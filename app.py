14e75b7cb0b8e4150ff81a3acddefe88c39d7a76
Rohan Pashikanti <140698619+rohanpashikanti@users.noreply.github.com>
2025-05-20 20:13:06 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,68 @@
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
+from pymongo import MongoClient
+from datetime import datetime
+
+app = Flask(__name__)
+CORS(app)
+
+# MongoDB Atlas Connection (replace with your credentials)
+client = MongoClient("mongodb+srv://rohanpash:rohanpash@mongotrail.pdqctpg.mongodb.net/?retryWrites=true&w=majority")
+db = client["anonymous"]  # Database name
+
+# Collections
+users_col = db["users"]
+messages_col = db["messages"]
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+# Register user
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    uid = data['uid']
+    email = data['email']
+    if not users_col.find_one({'uid': uid}):
+        users_col.insert_one({'uid': uid, 'email': email})
+    return jsonify({'message': 'User registered'}), 200
+
+# Get all users except current
+@app.route('/users', methods=['POST'])
+def get_users():
+    data = request.get_json()
+    current_uid = data['uid']
+    users = list(users_col.find({'uid': {'$ne': current_uid}}, {'_id': 0}))
+    return jsonify(users)
+
+# Store anonymous message with sender_uid
+@app.route('/send', methods=['POST'])
+def send_message():
+    data = request.get_json()
+    message = {
+        'sender_uid': data['sender'],
+        'receiver_uid': data['receiver'],
+        'content': data['content'],
+        'timestamp': datetime.utcnow()
+    }
+    messages_col.insert_one(message)
+    return jsonify({'message': 'Message sent'}), 200
+
+# Retrieve inbox (hide sender info)
+@app.route('/inbox', methods=['POST'])
+def inbox():
+    data = request.get_json()
+    uid = data['uid']
+    msgs = messages_col.find({'receiver_uid': uid}).sort('timestamp', -1)
+    result = []
+    for msg in msgs:
+        result.append({
+            'from': 'Anonymous',  # Sender hidden from user
+            'content': msg['content'],
+            'timestamp': msg['timestamp'].strftime('%Y-%m-%d %H:%M:%S')
+        })
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
